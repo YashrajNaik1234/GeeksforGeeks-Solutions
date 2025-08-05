38a1f9147abb3f47143a6af50fafbc5574288978
Raj <132782219+YashrajNaik1234@users.noreply.github.com>
2025-08-05 12:52:07 +0530

    Create Postfix to Infix.cpp

---
+++ Postfix to Infix.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    string postToInfix(string exp) {
+        // Write your code here
+        stack <string> st;
+        
+        for(int i(0);i < exp.size();i++){
+            if(isalnum(exp[i])) st.push(string(1, exp[i]));
+            else{
+                string a = st.top(); st.pop();
+                string b = st.top(); st.pop();
+                string c = '('+ b + exp[i] + a + ')';
+                st.push(c);
+            }
+        }
+        
+        return st.top();
+    }
+};
