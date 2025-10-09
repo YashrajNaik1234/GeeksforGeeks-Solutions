8c5d5bf768de8b86f61616f80b8e26d2d1bc6bb6
Raj <132782219+YashrajNaik1234@users.noreply.github.com>
2025-10-09 21:38:36 +0530

    Create Postorder Traversal.cpp

---
+++ Postorder Traversal.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    void solve(Node* root, vector<int>& ans){
+        if(!root) return;
+        solve(root -> left, ans);
+        solve(root -> right, ans);
+        ans.push_back(root -> data);
+    }
+    vector<int> postOrder(Node* root) {
+        // code here
+        vector<int> ans;
+        solve(root, ans);
+        return ans;
+    }
+};
