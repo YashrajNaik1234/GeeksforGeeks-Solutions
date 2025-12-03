9dec52dacb6dc9ce379aba7a55453a3582f7c4b4
Raj <132782219+YashrajNaik1234@users.noreply.github.com>
2025-12-03 14:47:45 +0530

    Update Frog Jump.cpp

--- Frog Jump.cpp
+++ Frog Jump.cpp
@@ -2,13 +2,11 @@ class Solution {
   public:
     int minCost(vector<int>& height) {
         // Code here
-         vector<int> dp(height.size(), 0);
+        vector<int> dp(height.size(), 0);
         
-        for(int i = height.size()-2;i>=0;i--){
-            int a = abs(height[i]-height[i+1]) + dp[i+1];
-            int b = INT_MAX;
+        for(int i = height.size() - 2;i >= 0;i--){
+            int a = abs(height[i] - height[i + 1]) + dp[i + 1], b = INT_MAX;
             if (i + 2 < height.size())  b = abs(height[i] - height[i + 2]) + dp[i + 2];
-            
             dp[i] = min(a, b);
         }
         
