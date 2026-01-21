d8693a9a5f2415d8d961859bd92baecddef2b40a
Raj <132782219+YashrajNaik1234@users.noreply.github.com>
2026-01-21 23:44:00 +0530

    Create Stock span problem.cpp

---
+++ Stock span problem.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    vector<int> calculateSpan(vector<int>& price) {
+        // code here
+        int n = price.size();
+        vector<int> span(n, 1);
+        stack<int> st; st.push(0);
+    
+        for (int i = 1; i < n; i++) {
+            while (!st.empty() && price[st.top()] <= price[i]) st.pop();
+            span[i] = st.empty() ? i + 1 : i - st.top(); st.push(i);   
+        }
+    
+        return span;
+    }
+};
